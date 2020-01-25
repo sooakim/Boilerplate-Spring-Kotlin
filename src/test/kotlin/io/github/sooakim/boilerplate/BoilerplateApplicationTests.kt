2672a931d164f0683968466c68bd1c70c55d74ea
SooaKim <alice2863@naver.com>
2020-01-25 20:30:58 +0900

    initial commit

---
+++ src/test/kotlin/io/github/sooakim/boilerplate/BoilerplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.sooakim.boilerplate
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BoilerplateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
